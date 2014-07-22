e86c32742728e368249da38e857a338fd1923f29
Marques Johansson <marques@displague.com>
2014-07-21 23:29:52 -0400

    slow conversion to sdl and an alternate form of fast direct pixel access - perhaps http://wiki.lazarus.freepascal.org/Fast_direct_pixel_access

--- scape.pas
+++ scape.pas
@@ -1,41 +1,15 @@
-{Programmed by Marques Johansson - PWAC 96 - pwac@flinet.com}
-
-{ This program may be distributed so long as my name is kept here...
-  If you add your own stuff- note it... I take no responsibility
-  for any damage :<   bla blah blarr....
-
-  Ok- i made this quick source use a paramline so that you can try
-  it w/ different pal files... if you don't have a good- FAST palette
-  writing program- try 256 paint- shareware written in pascal..
-  This prog. works very nice w/ red/blacks and sky colors- i also include
-  a landform palette- this would be good for a c&c type game for at
-  least one reason: instead of having grass and dirt icons to place
-  on the screen you use a landscape bg, it is quicker and will always
-  be the same... like point a  .       . and B
-   If a is altitude 10
-   and B is 30, than the middle will increment based on the space available.
-   if you zoom on this you will notice- especially if the difference in
-   altitude is great- more colors on more zooms- i should probably add
-   that feature to this demo- but then again- it is free, do it yourself.
-
-   If you would like me to try changing or adding something- go ahead
-   tell me- i'll probably try it...
-   there is one known bug- i haven't looked for it really hard though:
-    i think the corners of the screen bleed in artificial counts for
-    the averaging- when a random page 0-63 is made it should wind up
-    dithering somewhere about 32- well this seems to go straight to zero,
-    multiplication maybe? I don't care- i looks cool as is, if you
-    find the bug please let me know...}
-
-
-Uses dos;
+{Program Scape}
+{$linklib gcc}
+{$linklib SDLmain}
+Uses dos,
+     sdl;
 {$asmmode intel}
 {Procedure Scape;}
 
 Var x:   integer;
     y:   byte;
     h:   byte;
-    scr:   array [0..63999] Of byte absolute $a000;
+    scr:   PSDL_Surface;
     i,j,scans:   integer;
     a:   char;
     xvar,yvar:   integer;
@@ -66,12 +40,6 @@ Begin
     halt(1);
 End;
 
-Procedure setvideo(m : integer);
-assembler;
-asm
-mov ax,m;
-int 10h
-End;
 Procedure setpal(c,r,g,b : byte);
 assembler;
 asm
@@ -172,7 +140,12 @@ Begin;
 {if paramcount=0 then GiveHelp;}
     h := 0;
     Palfile := paramstr(1);
-    setvideo($13);
+    {setvideo($13);}
+    if SDL_Init( SDL_INIT_VIDEO ) < 0 then HALT;
+    scr:=SDL_SetVideoMode(320,200,24, SDL_SWSURFACE);
+    {sdlWindow1 := SDL_CreateWindow( 'Window1', 50, 50, 500, 500, SDL_WINDOW_SHOWN );
+        if sdlWindow1 = nil then HALT;}
+
     GrayPal();
   {Loadpal(PalFile);}
     For i:=0 To 255 Do
@@ -197,7 +170,7 @@ Begin;
                 Begin
                     jtemp := j*xvar;
                     For i:=1 To xvar-1 Do
-                        scr[i+jtemp+ytemp] := 
+                        scr[i+jtemp+ytemp] :=
                                               scr[ytemp+jtemp]+(((scr[(j+1)*xvar
                                               +ytemp]-scr[jtemp+ytemp])Div xvar)
                                               *i);
@@ -209,7 +182,7 @@ Begin;
                 Begin
                     jtemp := yvar*xmax;
                     For i:=1 To yvar-1 Do
-                        scr[(j*yvar+i)*xmax+x] := 
+                        scr[(j*yvar+i)*xmax+x] :=
                                                   scr[x+j*jtemp]+(((scr[(j+1)*
                                                   jtemp+x]-scr[j*jtemp+x]) Div
                                                   yvar)*i);
@@ -224,7 +197,9 @@ Begin;
             inc(scans);
         End;
     Until scans=scanreq;
-    setvideo($3);  {better remove this one to use as a sub}
+    {SDL_DestroyWindow ( sdlWindow1 );}
+    SDL_Quit;
+    {setvideo($3);}  {better remove this one to use as a sub}
 End.
 {Begin
  Scape;
