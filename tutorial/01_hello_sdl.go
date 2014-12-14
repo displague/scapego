15688a7036573058a64723f8315b81285cdbf01f
Marques Johansson <marques@displague.com>
2014-12-13 21:23:22 -0500

    implement lazyfoo.net tutorial in go

---
+++ tutorial/01_hello_sdl.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+var height = 600
+var width = 800
+var rows = 80
+var cols = 60
+
+func main() {
+	window, err := sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, width, height, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+
+	surface := window.GetSurface()
+
+	surface.FillRect(nil, sdl.MapRGB(surface.Format, 50, 50, 50))
+	window.UpdateSurface()
+	sdl.Delay(15000)
+	window.Destroy()
+	sdl.Quit()
+}
